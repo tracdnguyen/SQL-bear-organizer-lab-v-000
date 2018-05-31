45abc53f4d2acfbabf58aeb136ef0d84f85d526f
Trac Nguyen <nguyendtrac@gmail.com>
2018-05-31 20:31:13 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, temperament) VALUES ('Mr. Chocolate', 4, 'confident');
+INSERT INTO bears (name, age, temperament) VALUES ('Rowdy', 16, 'timid');
+INSERT INTO bears (name, age, temperament) VALUES ('Tabitha', 19, 'active');
+INSERT INTO bears (name, age, temperament) VALUES ('Sergeant Brown', 1, 'wild');
+INSERT INTO bears (name, age, temperament) VALUES ('Melissa', 5, 'agressive');
+INSERT INTO bears (name, age, temperament) VALUES ('Grinch', 2, 'calm');
+INSERT INTO bears (name, age, temperament) VALUES ('Wendy', 8, 'docile');
+INSERT INTO bears (name, age, temperament) VALUES (NULL, 13, 'crazy');
